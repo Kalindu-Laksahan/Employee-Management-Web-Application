62e210ca0bb7edd7aa0f58089cfaff9412235e69
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2022-12-27 16:06:57 +0530

    Completed Post Method

--- EmployeeManagement/Controllers/EmployeesControllers.cs
+++ EmployeeManagement/Controllers/EmployeesControllers.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using EmployeeManagement.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
 {
     public class EmployeesControllers : Controller
     {
+        private readonly MvcDbContext mvcDbContext;
+
+        public EmployeesControllers(MvcDbContext mvcDbContext)
+        {
+            this.mvcDbContext = mvcDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmpViewModel addEmpRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmpRequest.Name,
+                Email = addEmpRequest.Email,
+                Salary = addEmpRequest.Salary,
+                DateofBirth = addEmpRequest.DateofBirth,
+                Department = addEmpRequest.Department
+            };
+
+            await mvcDbContext.Employees.AddAsync(employee);
+            await mvcDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
         }
 
 
