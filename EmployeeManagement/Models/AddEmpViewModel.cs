537e8be6ebd0040f815668005b6b9ad198c4249f
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2022-12-27 15:54:06 +0530

    Created Add Employee View and UI

--- EmployeeManagement/Models/AddEmpViewModel.cs
+++ EmployeeManagement/Models/AddEmpViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class AddEmpViewModel
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+        public DateTime DateofBirth { get; set; }
     }
 }
