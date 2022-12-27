6aa5f649701e7a8f88f4c945fd6402c7e429fa30
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2022-12-27 15:28:17 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeesControllers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeesControllers : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ EmployeeManagement/Data/MvcDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class MvcDbContext : DbContext
+    {
+        //Constructor
+        public MvcDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
